e7da9876502cd2528aecc5d73e32f5cf3b307f38
Fekreselassie <fekruson@gmail.com>
2025-08-31 11:15:38 +0300

    fix react-native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -6,14 +6,13 @@ export default function Index() {
       <Text>Entry Screen - Awesome</Text>
       
       <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
