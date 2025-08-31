2bbc189f500374fb473a31da8ec070ab32f1d743
Fekreselassie <fekruson@gmail.com>
2025-08-31 11:18:12 +0300

    fix index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,12 +4,9 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-      
-      <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
 }
